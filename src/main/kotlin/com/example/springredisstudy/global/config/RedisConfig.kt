07f3431b2ca92477b4792ed0b2a38861758739ab
jeong-gumin <jgm@paytalab.com>
2023-01-29 22:56:17 +0900

    feat : redis 설정 추가

---
+++ src/main/kotlin/com/example/springredisstudy/global/config/RedisConfig.kt
@@ -0,0 +1,40 @@
+package com.example.springredisstudy.global.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+
+@Configuration
+class RedisConfig(
+    @Value("\${spring.redis.host}")
+    val host: String,
+    @Value("\${spring.redis.port}")
+    val port: String,
+    @Value("\${spring.redis.password}")
+    val password: String,
+) {
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration()
+        redisStandaloneConfiguration.hostName = host
+        redisStandaloneConfiguration.port = port.toInt()
+        redisStandaloneConfiguration.setPassword(password)
+        return LettuceConnectionFactory(redisStandaloneConfiguration)
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any> {
+        val redisTemplate = RedisTemplate<String, Any>()
+        redisTemplate.setConnectionFactory(redisConnectionFactory())
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = StringRedisSerializer()
+        return redisTemplate
+    }
+}
