a9f980ccaad3fc62e255c9e97a6416687304ef09
jeong-gumin <jgm@paytalab.com>
2023-01-30 00:28:36 +0900

    feat : 인증코드 요청 과다 예외 상태 변경

--- src/main/kotlin/com/example/springredisstudy/domain/exception/PhoneAuthTooManyRequestException.kt
+++ src/main/kotlin/com/example/springredisstudy/domain/exception/PhoneAuthTooManyRequestException.kt
@@ -4,6 +4,6 @@ import com.example.springredisstudy.global.exception.AbstractHttpStatusException
 import org.springframework.http.HttpStatus
 
 class PhoneAuthTooManyRequestException() : AbstractHttpStatusException(
-    HttpStatus.BAD_REQUEST,
+    HttpStatus.TOO_MANY_REQUESTS,
     "The mobile phone number verification code has been sent too many times."
 )
